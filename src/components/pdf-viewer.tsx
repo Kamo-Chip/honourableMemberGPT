13c85f97bdb9bc41cc3e89b3b2f440ce7b99694f
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2024-06-15 22:46:10 +0200

    fix - Removed react-pdf and used iframe

--- src/components/pdf-viewer.tsx
+++ src/components/pdf-viewer.tsx
@@ -1,26 +1,26 @@
 "use client";
 
-import { pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+// import { pdfjs } from "react-pdf";
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/build/pdf.worker.min.mjs",
+//   import.meta.url
+// ).toString();
 
-import { useState } from "react";
-import { Document, Page } from "react-pdf";
+// import { useState } from "react";
+// import { Document, Page } from "react-pdf";
 
 export const PdfViewer = ({ documentUrl }: { documentUrl: string }) => {
-  const [numPages, setNumPages] = useState<number>();
+  // const [numPages, setNumPages] = useState<number>();
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
+  // function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+  //   setNumPages(numPages);
+  // }
 
   if (!documentUrl) return <span className="m-auto">No file to display</span>;
 
   return (
     <div className="max-w-full overflow-hidden rounded-lg shadow-sm border-solid border-[1px] h-full">
-      <Document file={documentUrl} onLoadSuccess={onDocumentLoadSuccess}>
+      {/* <Document file={documentUrl} onLoadSuccess={onDocumentLoadSuccess}>
         {Array.apply(null, Array(numPages))
           .map((x, i) => i + 1)
           .map((page) => (
@@ -31,7 +31,8 @@ export const PdfViewer = ({ documentUrl }: { documentUrl: string }) => {
               renderAnnotationLayer={false}
             />
           ))}
-      </Document>
+      </Document> */}
+      <iframe src={documentUrl} className="w-full h-full" />
     </div>
   );
 };
