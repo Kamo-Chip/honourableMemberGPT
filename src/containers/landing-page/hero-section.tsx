9d2648cda8599d87a74d0f9f5f55e7bde76457cc
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2024-06-18 21:59:02 +0200

    style - minor changes

--- src/containers/landing-page/hero-section.tsx
+++ src/containers/landing-page/hero-section.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="flex flex-col text-6xl max-w-3xl text-center md:text-8xl leading-tight mt-14">
+    <section className="flex flex-col text-6xl max-w-3xl text-center md:text-8xl leading-tight mt-28">
       <h1 className="font-medium">Chat with Political Party Manifestos</h1>
       <p className="text-base mt-4 font-light md:text-lg leading-relaxed">
         An AI chatbot that makes you an informed voter by helping you understand
