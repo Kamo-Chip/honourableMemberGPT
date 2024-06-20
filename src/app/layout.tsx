192ecf64425125d996528ba7f6931c11d26d138c
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2024-06-20 19:06:43 +0200

    style - metadata stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,6 @@ export const metadata: Metadata = {
   description:
     "An AI chatbot that makes you an informed citizen by giving you the TLDR of political party manifestos",
   viewport: "width=device-width, initial-scale=1.0",
-  openGraph: {
-    images: ["/images/og-image.png"],
-  },
 };
 
 export default function RootLayout({
