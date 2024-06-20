6a96ffcac68ce011957815c7e850a1f8fe551308
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2024-06-20 19:18:25 +0200

    style - metadata stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,31 @@
 import MaxWidthWrapper from "@/components/wrappers/max-width-wrapper";
 import type { Metadata } from "next";
 import "./globals.css";
+import ogImage from "./opengraph-image.png";
 
 export const metadata: Metadata = {
   title: "HonourableMemberGPT",
   description:
     "An AI chatbot that makes you an informed citizen by giving you the TLDR of political party manifestos",
   viewport: "width=device-width, initial-scale=1.0",
+  openGraph: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
